6b5c0cf39409135c8b1424447aca224a0d4361d7
pip64 <76626915+pip64@users.noreply.github.com>
2025-10-06 19:09:38 +0700

    Added websocket reconnect if app became active from inactive

--- src/providers/WebSocketContext.js
+++ src/providers/WebSocketContext.js
@@ -1,6 +1,6 @@
 import { createSecureStorage } from "@lib/Storage";
 import { createContext, useContext, useEffect, useRef, useState } from "react";
-import setChatKeysToStorage from "@lib/setChatKeysToStorage";
+import { AppState } from "react-native";
 import { WEBSOCKET_URL } from "@constants/Api";
 
 const WebSocketContext = createContext(null);
@@ -8,10 +8,10 @@ const WebSocketContext = createContext(null);
 export const WebSocketProvider = ({ children }) => {
     const [socket, setSocket] = useState(null);
     const reconnectTimeout = useRef(null);
+    const appState = useRef(AppState.currentState);
 
     const connect = async () => {
         const storage = await createSecureStorage("user-storage");
-
         const ws = new WebSocket(WEBSOCKET_URL + storage.getString("token"));
 
         ws.onopen = () => {
@@ -22,10 +22,11 @@ export const WebSocketProvider = ({ children }) => {
         };
 
         ws.onclose = () => {
-            reconnectTimeout.current = setTimeout(connect, 3000);
+            if (!reconnectTimeout.current)
+                reconnectTimeout.current = setTimeout(connect, 3000);
         };
 
-        ws.onerror = (err) => {
+        ws.onerror = () => {
             ws.close();
         };
 
@@ -34,8 +35,26 @@ export const WebSocketProvider = ({ children }) => {
 
     useEffect(() => {
         connect();
+
+        const subscription = AppState.addEventListener("change", (nextState) => {
+            if (
+                appState.current.match(/active/) &&
+                nextState.match(/inactive|background/)
+            ) {
+                if (socket) socket.close();
+            } else if (
+                appState.current.match(/inactive|background/) &&
+                nextState === "active"
+            ) {
+                connect();
+            }
+            appState.current = nextState;
+        });
+
         return () => {
             if (reconnectTimeout.current) clearTimeout(reconnectTimeout.current);
+            if (socket) socket.close();
+            subscription.remove();
         };
     }, []);
 
