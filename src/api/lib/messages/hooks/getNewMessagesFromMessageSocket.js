b8bf92ce0c007521c71bb4ed3484400fe05b1e72
pip64 <76626915+pip64@users.noreply.github.com>
2025-11-11 20:32:43 +0700

    fixed stupid mistake

--- src/api/lib/messages/hooks/getNewMessagesFromMessageSocket.js
+++ src/api/lib/messages/hooks/getNewMessagesFromMessageSocket.js
@@ -2,15 +2,20 @@ import mergeAndSort from "@api/lib/utils/mergeAndSort";
 import filterMessagesByChatId from "../filterMessagesByChatId";
 
 export default function (mmkv, setMessages, newMessages, chat_id, messages, clearNewMessages) {
-    if (!newMessages?.length) return;
+    try {
+        if (!newMessages?.length) return;
 
-    // filter messages by current chat_id
-    const filtered = filterMessagesByChatId(mmkv, chat_id, messages);
+        // filter messages by current chat_id
+        const filtered = filterMessagesByChatId(mmkv, chat_id, newMessages);
 
-    if (filtered.length > 0) {
-        setMessages(prev => mergeAndSort(prev, filtered));
+        if (filtered.length > 0) {
+            setMessages(prev => mergeAndSort(prev, filtered));
+        }
+
+        // clear context messages history
+        clearNewMessages();
+    } catch (error) {
+        console.log(error)
     }
 
-    // clear context messages history
-    clearNewMessages();
 }
