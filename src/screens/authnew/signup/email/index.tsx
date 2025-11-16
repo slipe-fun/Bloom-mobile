c76b4097bad1b3c4fc34b18a9948edf6c67a7f62
Dikiy Perec <hopabsq@mail.ru>
2025-11-16 17:31:37 +0600

    Added mailBoxes logos for auth

--- src/screens/authnew/signup/email/index.tsx
+++ src/screens/authnew/signup/email/index.tsx
@@ -4,6 +4,8 @@ import { styles } from "./Email.styles";
 
 export default function SignUpEmail(): React.JSX.Element {
     return (
-        <View style={styles.container}></View>
+        <View style={styles.container}>
+            
+        </View>
     )
 }
