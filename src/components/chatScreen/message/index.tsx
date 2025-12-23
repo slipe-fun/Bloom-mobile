aa7c481cd8e2e4a197fd4cb1f2e6367033cad0c6
dikiyp <hopabsq@mail.ru>
2025-12-24 03:04:25 +0600

    Removed some necessary code from chat screen

--- src/components/chatScreen/message/index.tsx
+++ src/components/chatScreen/message/index.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Pressable } from "react-native";
 import { styles } from "./Message.styles";
-import Animated, { AnimatedRef, Easing, FadeIn, LayoutAnimationConfig, SharedValue, useAnimatedRef, useSharedValue, withTiming } from "react-native-reanimated";
+import Animated, { LayoutAnimationConfig } from "react-native-reanimated";
 import type { Message } from "@interfaces";
 import MessageBubble from "./MessageBubble";
 import MessageStatus from "./MessageStatus";
+import { getFadeIn } from "@constants/animations";
 
 type MessageProps = {
 	message: Message | null;
@@ -12,18 +13,16 @@ type MessageProps = {
 	isLast?: boolean;
 	isGroupStart?: boolean,
 	isGroupEnd?: boolean;
-	messageRef?: AnimatedRef<Animated.View>;
 };
 
 const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 
-export default function Message({ message, seen, isLast, isGroupStart, isGroupEnd, messageRef }: MessageProps): React.JSX.Element {
+export default function Message({ message, seen, isLast, isGroupStart, isGroupEnd }: MessageProps): React.JSX.Element {
 
 	return (
 		<AnimatedPressable
-			entering={FadeIn}
-			ref={messageRef}
-			style={styles.messageWrapper(message?.isMe)}
+			entering={getFadeIn()}
+			style={[styles.messageWrapper(message?.isMe)]}
 		>
 			<MessageBubble message={message} />
 			<LayoutAnimationConfig skipEntering skipExiting>
--- src/screens/chat/index.tsx
+++ src/screens/chat/index.tsx
@@ -9,114 +9,107 @@ import { useReanimatedKeyboardAnimation } from "react-native-keyboard-controller
 import useMessages from "@api/hooks/encryption/useMessages";
 import type { Chat, Message as MessageType } from "@interfaces";
 import { useScreenScale } from "@hooks";
-import { KeyboardAvoidingLegendList} from "@legendapp/list/keyboard"
+import { KeyboardAvoidingLegendList } from "@legendapp/list/keyboard";
 import { LegendListRef } from "@legendapp/list";
 
 interface ChatScreenProps {
-	route: {
-		params: Object;
-	};
+  route: {
+    params: Object;
+  };
 }
 
 export default function ChatScreen({ route }: ChatScreenProps): React.JSX.Element {
-	const { chat } = route.params as { chat: Chat };
+  const { chat } = route.params as { chat: Chat };
 
-	const { messages, addMessage } = useMessages(chat?.id);
-	const [seenId, setSeenId] = useState<number>(0);
-	const [footerHeight, setFooterHeight] = useState<number>(0);
-	const [headerHeight, setHeaderHeight] = useState<number>(0);
-	const [lastMessageId, setLastMessageId] = useState<number>(0);
-	const { animatedScreenStyle } = useScreenScale();
-	const { height: keyboardHeight, progress: keyboardProgress } = useReanimatedKeyboardAnimation();
-	const listRef = useRef<LegendListRef>(null);
-	
-	const CHAT_TIME_WINDOW = 5 * 60 * 1000;
+  const { messages, addMessage } = useMessages(chat?.id);
+  const [seenId, setSeenId] = useState<number>(0);
+  const [footerHeight, setFooterHeight] = useState<number>(0);
+  const [headerHeight, setHeaderHeight] = useState<number>(0);
+  const [lastMessageId, setLastMessageId] = useState<number>(0);
+  const { animatedScreenStyle } = useScreenScale();
+  const { height: keyboardHeight } = useReanimatedKeyboardAnimation();
+  const listRef = useRef<LegendListRef>(null);
 
-	useEffect(() => {
-		let lastSeen = 0;
-		for (let i = messages.length - 1; i >= 0; i--) {
-			const m = messages[i];
-			if (m?.seen && m?.isMe) {
-				lastSeen = m.id;
-				break;
-			}
-		}
-		setSeenId(lastSeen);
-		setLastMessageId(messages.length ? messages[messages.length - 1]?.id : 0);
-	}, [messages.length, messages]);
+  const CHAT_TIME_WINDOW = 5 * 60 * 1000;
 
-	const renderItem = useCallback(
-        ({ item, index }: { item: MessageType; index: number }) => {
-			if (item?.type === "date_header") {
-				// бейдж даты
-				// жсон выглядbт так
-				// {"text": "13 декабря", "type": "date_header"}
-				return;
-			}
+  useEffect(() => {
+    let lastSeen = 0;
+    for (let i = messages.length - 1; i >= 0; i--) {
+      const m = messages[i];
+      if (m?.seen && m?.isMe) {
+        lastSeen = m.id;
+        break;
+      }
+    }
+    setSeenId(lastSeen);
+    setLastMessageId(messages.length ? messages[messages.length - 1]?.id : 0);
+  }, [messages.length, messages]);
 
-            const prevItem = messages[index - 1];
-            const nextItem = messages[index + 1];
+  const renderItem = useCallback(
+    ({ item, index }: { item: MessageType; index: number }) => {
+      if (item?.type === "date_header") {
+        // бейдж даты
+        // жсон выглядbт так
+        // {"text": "13 декабря", "type": "date_header"}
+        return;
+      }
 
-            const isGroupStart = !prevItem 
-                || prevItem.author_id !== item.author_id 
-                || (new Date(item.date).getTime() - new Date(prevItem.date).getTime() > CHAT_TIME_WINDOW);
+      const prevItem = messages[index - 1];
+      const nextItem = messages[index + 1];
 
-            const isGroupEnd = !nextItem 
-                || nextItem.author_id !== item.author_id 
-                || (new Date(nextItem.date).getTime() - new Date(item.date).getTime() > CHAT_TIME_WINDOW);
+      const isGroupStart =
+        !prevItem ||
+        prevItem.author_id !== item.author_id ||
+        new Date(item.date).getTime() - new Date(prevItem.date).getTime() > CHAT_TIME_WINDOW;
 
-            return (
-                <Message
-                    key={item?.nonce}
-                    seen={seenId === item?.id}
-                    isLast={lastMessageId === item?.id}
-                    message={item}
-                    isGroupStart={isGroupStart}
-                    isGroupEnd={isGroupEnd}
-                />
-            );
-        },
-        [seenId, lastMessageId, footerHeight, messages] 
-    );
+      const isGroupEnd =
+        !nextItem ||
+        nextItem.author_id !== item.author_id ||
+        new Date(nextItem.date).getTime() - new Date(item.date).getTime() > CHAT_TIME_WINDOW;
 
-		const ss = () => {
-			listRef.current?.scrollToEnd({ animated: true});
-	}
+      return (
+        <Message
+          key={item?.nonce}
+          seen={seenId === item?.id}
+          isLast={lastMessageId === item?.id}
+          message={item}
+          isGroupStart={isGroupStart}
+          isGroupEnd={isGroupEnd}
+        />
+      );
+    },
+    [seenId, lastMessageId, footerHeight, messages, footerHeight]
+  );
 
-	useEffect(() => {
-		
-		ss()
-	}, [messages.length])
+  const ss = () => {
+    listRef.current?.scrollToEnd({ animated: true, viewOffset: keyboardHeight.get() });
+  };
 
+  useEffect(() => {
+    ss();
+  }, [messages.length]);
 
-	// useAnimatedReaction(() => keyboardProgress.value, (current, previous) => {
-	// 	if (current > 0) {
-	// 		if (isUserNearBottom.get()) runOnJS(ss)(1)
-	// 	}
-	// })
+  const keyExtractor = useCallback((item: MessageType, index: number) => {
+    return String(item?.nonce ?? item?.id ?? index);
+  }, []);
 
-	const keyExtractor = useCallback((item: MessageType, index: number) => {
-		return String(item?.nonce ?? item?.id ?? index);
-
-	}, []);
-	return (
-		<View style={[styles.container, animatedScreenStyle]}>
-			<Header onLayout={setHeaderHeight} chat={chat} />
-			<EmptyModal chat={chat} visible={messages.length === 0} />
-				<KeyboardAvoidingLegendList
-					data={messages}
-					renderItem={renderItem}
-					alignItemsAtEnd
-					ref={listRef}
-					keyExtractor={keyExtractor}
-					// maintainScrollAtEnd
-					style={styles.list}
-					contentInset={{ bottom: footerHeight -16, top: 0}}
-					contentContainerStyle={[styles.listContent]}
-					keyboardDismissMode="interactive"
-					showsVerticalScrollIndicator={false}
-				/>
-				<Footer setFooterHeight={setFooterHeight} footerHeight={footerHeight} onSend={addMessage} />
-		</View>
-	);
+  return (
+    <View style={[styles.container, animatedScreenStyle]}>
+      <Header onLayout={setHeaderHeight} chat={chat} />
+      <EmptyModal chat={chat} visible={messages.length === 0} />
+      <KeyboardAvoidingLegendList
+        data={messages}
+        renderItem={renderItem}
+        alignItemsAtEnd
+        ref={listRef}
+        keyExtractor={keyExtractor}
+        style={styles.list}
+        contentInset={{ bottom: footerHeight, top: headerHeight }}
+        contentContainerStyle={[styles.listContent]}
+        keyboardDismissMode='interactive'
+        showsVerticalScrollIndicator={false}
+      />
+      <Footer setFooterHeight={setFooterHeight} footerHeight={footerHeight} onSend={addMessage} />
+    </View>
+  );
 }
