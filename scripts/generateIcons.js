a21a2ac4709d68f234dd164c7785a48e96f17613
Dikiy Perec <hopabsq@mail.ru>
2026-01-06 01:20:08 +0600

    Updated script

--- scripts/generateIcons.js
+++ scripts/generateIcons.js
@@ -1,7 +1,7 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
+import * as fs from 'node:fs'
+import * as path from 'node:path'
+import { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
