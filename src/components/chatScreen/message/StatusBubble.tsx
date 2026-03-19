d625d1ce81130665e7d98be512164ab020565be9
dikiyp <hopabsq@mail.ru>
2026-03-20 01:16:00 +0600

    Add swipeable bubble for replies (W.I.P)

--- src/components/chatScreen/message/StatusBubble.tsx
+++ src/components/chatScreen/message/StatusBubble.tsx
@@ -1,11 +1,35 @@
-import Animated from 'react-native-reanimated'
+import { quickSpring } from '@constants/easings'
+import type { ViewStyle } from 'react-native'
+import Animated, { interpolate, type SharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated'
 import { styles } from './Message.styles'
 
 interface StatusBubbleProps {
   seen: boolean
   isMe: boolean
+  swipeX: SharedValue<number>
+  hapticsTriggered: SharedValue<boolean>
 }
 
-export default function StatusBubble({ isMe, seen }: StatusBubbleProps) {
-  return <Animated.View style={styles.statusBubble(isMe)} />
+export default function StatusBubble({ isMe, seen, swipeX, hapticsTriggered }: StatusBubbleProps) {
+  const animatedStyle = useAnimatedStyle((): ViewStyle => {
+    const scale = interpolate(swipeX.get(), [-60, 0], [4.5, 1], 'clamp')
+    const translateX = interpolate(swipeX.get(), [-60, 0], [4, 0], 'clamp')
+
+    return {
+      transform: [{ scale }, { translateX }],
+    }
+  })
+
+  const animatedEffectStyle = useAnimatedStyle((): ViewStyle => {
+    const scale = withSpring(hapticsTriggered.get() ? 4 : 1, quickSpring)
+    const translateX = interpolate(swipeX.get(), [-60, 0], [4, 0], 'clamp')
+    const opacity = withSpring(hapticsTriggered.get() ? 0 : 1, quickSpring)
+
+    return {
+      opacity,
+      transform: [{ scale }, { translateX }],
+    }
+  })
+
+  return <Animated.View style={[styles.statusBubble(isMe), animatedStyle]}></Animated.View>
 }
--- src/components/chatScreen/message/index.tsx
+++ src/components/chatScreen/message/index.tsx
@@ -1,5 +1,10 @@
+import { springy } from '@constants/animations'
 import type { Message as MessageType } from '@interfaces'
 import { Pressable } from 'react-native'
+import { Gesture, GestureDetector } from 'react-native-gesture-handler'
+import { Haptics } from 'react-native-nitro-haptics'
+import { NitroModules } from 'react-native-nitro-modules'
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated'
 import MessageBubble from './Bubble'
 import { styles } from './Message.styles'
 import StatusBubble from './StatusBubble'
@@ -11,11 +16,52 @@ interface MessageProps {
   shouldAnimate: boolean
 }
 
+const AnimatedPressable = Animated.createAnimatedComponent(Pressable)
 export default function Message({ message, seen, marginBottom, shouldAnimate }: MessageProps) {
+  const swipeX = useSharedValue(0)
+  const hapticTriggered = useSharedValue(false)
+
+  const boxed = NitroModules.box(Haptics)
+
+  const panGesture = Gesture.Pan()
+    .activeOffsetX([-10, 10])
+    .onStart(() => {
+      hapticTriggered.value = false
+    })
+    .onUpdate((event) => {
+      'worklet'
+      const x = event.translationX
+
+      if (x < -60) {
+        swipeX.value = -60 + (x + 60) * 0.125
+
+        if (!hapticTriggered.value) {
+          boxed.unbox().impact('heavy')
+          hapticTriggered.value = true
+        }
+      } else if (x > 0) {
+        swipeX.value = x * 0.125
+      } else {
+        // Обычное движение
+        swipeX.value = x
+      }
+    })
+    .onEnd(() => {
+      swipeX.value = withSpring(0, springy)
+    })
+
+  const animatedMessageStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: swipeX.value }],
+    }
+  })
+
   return (
-    <Pressable style={[styles.messageWrapper(message?.isMe, marginBottom)]}>
-      <StatusBubble isMe={message?.isMe} seen={seen} />
-      <MessageBubble message={message} seen={seen} shouldAnimate={shouldAnimate} />
-    </Pressable>
+    <GestureDetector gesture={panGesture}>
+      <AnimatedPressable style={[styles.messageWrapper(message?.isMe, marginBottom), animatedMessageStyle]}>
+        <StatusBubble hapticsTriggered={hapticTriggered} isMe={message?.isMe} seen={seen} swipeX={swipeX} />
+        <MessageBubble message={message} seen={seen} shouldAnimate={shouldAnimate} />
+      </AnimatedPressable>
+    </GestureDetector>
   )
 }
