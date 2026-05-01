4c4227ad1faf7c55d553a58d6c6b1b0fc394729d
dikiyp <hopabsq@mail.ru>
2026-05-01 17:53:17 +0600

    Add confetti

--- app/(auth)/Success.tsx
+++ app/(auth)/Success.tsx
@@ -1,13 +1,28 @@
+import { useFocusEffect } from 'expo-router'
+import { useCallback, useRef } from 'react'
 import { View } from 'react-native'
+import { Confetti, type ConfettiMethods } from 'react-native-fast-confetti'
 import { StyleSheet } from 'react-native-unistyles'
 
 export default function Success() {
-  return <View style={styles.container}></View>
+  const confettiRef = useRef<ConfettiMethods>(null)
+
+  useFocusEffect(
+    useCallback(() => {
+      confettiRef.current?.restart()
+    }, []),
+  )
+  return (
+    <View style={styles.container}>
+      <Confetti ref={confettiRef} count={150} fadeOutOnEnd />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create((theme) => ({
   container: {
     flex: 1,
+    zIndex: 1,
     justifyContent: 'flex-end',
     backgroundColor: theme.colors.background,
   },
