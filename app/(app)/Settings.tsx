3846b1d5e128e513b63d75c89f96a181e5e32867
dikiyp <hopabsq@mail.ru>
2026-05-06 01:32:04 +0600

    Improve settings animation a lot

--- app/(app)/Settings.tsx
+++ app/(app)/Settings.tsx
@@ -6,8 +6,8 @@ import { StyleSheet } from 'react-native-unistyles'
 export default function Settings() {
   return (
     <View style={styles.container}>
-      <Transition.Boundary.View id="avatar">
-        <Avatar style={styles.avatar} size="xl" userId="dk3k293KK" />
+      <Transition.Boundary.View style={{ width: 100, height: 100 }} id="avatar">
+        <Avatar style={styles.avatar} size="2xl" userId="dk3k293KK" />
       </Transition.Boundary.View>
       <Text style={styles.title}>Settings</Text>
     </View>
@@ -17,7 +17,10 @@ export default function Settings() {
 const styles = StyleSheet.create((theme) => ({
   container: {
     flex: 1,
-    backgroundColor: theme.colors.background,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    boxShadow: `${theme.shadows.pressable} ${theme.colors.shadow}`,
+    paddingTop: 80,
   },
   title: {
     fontSize: theme.fontSize.lg,
--- layouts/zoomTransition.ts
+++ layouts/zoomTransition.ts
@@ -1,25 +1,49 @@
 import { quickSpring } from '@constants/easings'
 import { staticColors } from '@design/colors'
+import { Platform } from 'react-native'
 import { interpolate } from 'react-native-reanimated'
+import { NAVIGATION_MASK_ELEMENT_STYLE_ID } from 'react-native-screen-transitions'
 import type { BlankStackNavigationOptions } from 'react-native-screen-transitions/blank-stack'
-import { UnistylesRuntime } from 'react-native-unistyles'
 
 export const TOP_OFFSET = 4
 
 export const zoomTransition = (): BlankStackNavigationOptions => {
-  const color = UnistylesRuntime.getTheme().colors.background
-
   return {
-    // experimental_enableHighRefreshRate: true,
+    experimental_enableHighRefreshRate: true,
     gestureEnabled: true,
+    gestureDrivesProgress: false,
     gestureDirection: 'bidirectional',
-    screenStyleInterpolator: ({ bounds }) => {
+    gestureReleaseVelocityScale: 1.3,
+    navigationMaskEnabled: Platform.OS === 'ios',
+    screenStyleInterpolator: ({ bounds, insets, progress, focused }) => {
       'worklet'
 
-      return bounds({ id: 'avatar' }).navigation.zoom({
-        target: 'fullscreen',
-        debug: true,
+      const opacity = interpolate(progress, [0, 1, 2], [0, 0.25, 0], 'clamp')
+      const radius = !focused ? 0 : insets.top - TOP_OFFSET
+
+      const navigationStyles = bounds({
+        id: 'avatar',
+      }).navigation.zoom({
+        target: 'bound',
+        borderRadius: radius,
+        backgroundScale: 1,
+        horizontalDragScale: [0.75, 1.3, 1.5],
+        verticalDragScale: [0.75, 1.3, 1.5],
+        horizontalDragTranslation: [1, 1, 0],
+        verticalDragTranslation: [1, 1, 0],
       })
+
+      return {
+        ...navigationStyles,
+        [NAVIGATION_MASK_ELEMENT_STYLE_ID]: {
+          ...navigationStyles[NAVIGATION_MASK_ELEMENT_STYLE_ID],
+          borderCurve: 'continuous',
+        },
+        backdrop: {
+          opacity: opacity,
+          backgroundColor: staticColors.black,
+        },
+      }
     },
     transitionSpec: {
       open: quickSpring,
