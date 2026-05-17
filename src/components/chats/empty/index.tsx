d2e6b3ad863fe3812e14515996e49a7b3e859c88
dikiyp <hopabsq@mail.ru>
2026-05-17 19:55:12 +0600

    Add locales

--- src/components/chats/empty/index.tsx
+++ src/components/chats/empty/index.tsx
@@ -1,4 +1,5 @@
 import { getFadeOut, reversedZoomAnimationIn } from '@constants/animations'
+import { useTranslation } from 'react-i18next'
 import { Text, View } from 'react-native'
 import { useReanimatedKeyboardAnimation } from 'react-native-keyboard-controller'
 import Animated, { useAnimatedStyle } from 'react-native-reanimated'
@@ -7,6 +8,7 @@ import ListShuffle from './listShuffle'
 
 export default function Empty() {
   const keyboard = useReanimatedKeyboardAnimation()
+  const { t } = useTranslation('common')
 
   const animatedStyle = useAnimatedStyle(() => {
     return { transform: [{ translateY: keyboard.height.value / 2 }] }
@@ -15,8 +17,8 @@ export default function Empty() {
     <Animated.View entering={reversedZoomAnimationIn} exiting={getFadeOut()} style={[styles.container, animatedStyle]}>
       <ListShuffle />
       <View style={styles.textContainer}>
-        <Text style={styles.title}>It's quiet here</Text>
-        <Text style={styles.subTitle}>Start a conversation with your friends here!</Text>
+        <Text style={styles.title}>{t('common:chats.empty.title')}</Text>
+        <Text style={styles.subTitle}>{t('common:chats.empty.subTitle')}</Text>
       </View>
     </Animated.View>
   )
